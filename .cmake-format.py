97452f057a1e2dcf36115a44fd64f1001f109481
tayne3 <tayne3@dingtalk.com>
2025-11-25 09:25:42 +0800

    chore(cmake): Add CMake package configuration, formatting, and refine build options and external dependency management.

---
+++ .cmake-format.py
@@ -0,0 +1,8 @@
+with section("format"):
+  line_width = 120
+  tab_size = 2
+  separate_ctrl_name_with_space = False
+  separate_fn_name_with_space = False
+  dangle_parens = True
+  line_ending = unix
+  keyword_case = unchanged
